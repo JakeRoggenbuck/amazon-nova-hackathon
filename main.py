a742263a953715d3c56bdbf8f19291c54dd91da7
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2026-03-09 23:58:53 -0700

    Revert back to basic main

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-from openai import OpenAI
-
-with open("key.secret") as file:
-    KEY = file.read().rstrip()
-
-client = OpenAI(api_key=KEY, base_url="https://api.nova.amazon.com/v1")
-
-response = client.chat.completions.create(
-    model="nova-2-lite-v1",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": "Hello! How are you?"},
-    ],
-    stream=False,
-)
-
-print(response.choices[0].message.content)
+print("Hey Kyle")
